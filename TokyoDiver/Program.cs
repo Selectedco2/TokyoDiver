d09818ece4d2e95eec2ceabd21648ab6e898a7c3
Andreas Thegelström <andreas.thegelstrom@elev.ga.ntig.sem>
2024-03-07 11:01:54 +0100

    Gjorde en klass för olika tjejer

--- TokyoDiver/Program.cs
+++ TokyoDiver/Program.cs
@@ -3,8 +3,43 @@
 ====================================================================================================*/
 string DefaultName = "Jack";
 
+// your bestfriend (crush on you)
+Girl asuka = new Girl();
+asuka.name = "Asuka";
+asuka.affection = 2;
+
+// cosplaying shy girl
+Girl mai = new Girl();
+mai.name = "Mai";
+mai.affection = 0;
+
+// nerdy library girl
+Girl akane = new Girl();
+akane.name = "Akane";
+akane.affection = 0;
+
+// stern independet girl
+Girl maiko = new Girl();
+maiko.name = "Maiko";
+maiko.affection = -1;
 
 
 /*====================================================================================================
     Gamestart
 ====================================================================================================*/
+
+
+
+
+
+
+/*====================================================================================================
+    Klasser
+====================================================================================================*/
+
+class Girl
+{
+    public string name;
+    public int affection;
+
+}
